9b6eb6704753c167f32bed0f822fa60a3c6df00f
John Smith <js4218@gmail.com>
2024-06-25 20:01:49 +0000

    Fixed shaders

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,10 +1,16 @@
+// Use the GLSL ES 1.00 version directive
 #version 100
 
+// Precision qualifier
 precision mediump float;
 
+// Varying variables (input from vertex shader)
 varying vec2 vTexcoord;
+
+// Sampler for the texture
 uniform sampler2D texture0;
 
 void main() {
+    // Output the color of the texture at the given coordinates
     gl_FragColor = texture2D(texture0, vTexcoord);
 }
--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,11 +1,17 @@
+// Use the GLSL ES 1.00 version directive
 #version 100
 
+// Attribute variables (input to vertex shader)
 attribute vec3 position;
 attribute vec2 texcoord;
 
+// Varying variables (output from vertex shader, input to fragment shader)
 varying vec2 vTexcoord;
 
 void main() {
+    // Pass the texture coordinates to the fragment shader
     vTexcoord = texcoord;
+
+    // Set the position of the current vertex
     gl_Position = vec4(position, 1.0);
 }
